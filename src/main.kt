4e97228abce53f5c2fea7441f125aeed48f28141
Akuot-philip <akuotphilipp@gmail.com>
2022-03-10 08:55:57 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,33 @@
+fun main(){
+    var programme ="codeHive"
+    var char = programme  [4]
+    var char1 = programme [5]
+    var char2 =programme  [6]
+    var char3 =programme  [7]
+    var text ="$char$char1$char2$char3"
+    println(text.toString())
+
+    println(remainder)
+    var x =stmt("Akuot", z = 21)
+    println(x)
+
+    var mother ="cecilia"
+    println(mother.length)
+
+
+}
+var a= 17
+var b= 5
+var remainder =a % b
+
+
+
+fun stmt (p:String,z:Int):String{
+    var stmt =("Hi,my name is $p and i am $z years old")
+    return stmt
+
+
+}
+fun mother(){
+    var name ="marcedez benz"
+}
